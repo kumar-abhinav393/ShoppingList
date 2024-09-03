5e327cf2812f115b95688984666d6c47c908e097
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2024-09-03 13:05:22 +0200

    Adding a new source file

---
+++ app/src/main/java/de/cispa/shoppinglist/ShoppingList.kt
@@ -0,0 +1,9 @@
+package de.cispa.shoppinglist
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ShoppingList(){
+    Text(text = "Welcome to the world of Android Programming!")
+}
