57c17ad141eeeadb8d01cdadb003574857de5f6c
Abhinav Abhinav <abhinav.abhinav@realcore.de>
2024-09-04 17:36:10 +0200

    Adding some new features

--- app/src/main/java/de/cispa/shoppinglist/ShoppingList.kt
+++ app/src/main/java/de/cispa/shoppinglist/ShoppingList.kt
@@ -1,16 +1,26 @@
 package de.cispa.shoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 
 data class shoppingList (
@@ -35,7 +48,6 @@ fun ShoppingList(){
     var inputItemName by remember { mutableStateOf("") }
     var inputItemQuantity by remember { mutableStateOf("1") }
     var sItems by remember { mutableStateOf(listOf<shoppingList>()) }
-    var shopList = shoppingList(101, "Abhinav", 1)
 
     //The below code is to let users enter only values in quantity text box
     val onValueChangeQty: (String) -> Unit = {newText ->
@@ -54,6 +66,33 @@ fun ShoppingList(){
         Spacer(
             modifier = Modifier.height(16.dp)
         )
+        LazyColumn(modifier = Modifier.fillMaxSize()){
+            items(sItems){item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                        .border(
+                            border = BorderStroke(2.dp, Color.Black),
+                            shape = RoundedCornerShape(20)
+                        )
+                        .padding(8.dp, 8.dp, 8.dp, 8.dp)
+                ) {
+                    Text(text = item.name, Modifier.padding(8.dp))
+                    Text(text = "Qty: ${item.quantity}", Modifier.padding(8.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.Edit, contentDescription = "")
+                        }
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(imageVector = Icons.Default.Delete, contentDescription = "")
+                        }
+                    }
+                }
+            }
+        }
     }
     if(showDialog){
         AlertDialog(onDismissRequest = { showDialog = false }, confirmButton = { 
